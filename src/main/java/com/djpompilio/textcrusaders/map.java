4cd95f34729628437fc47bd0f6653795d88aa290
djpompilio <djpompilio@gmail.com>
2025-05-31 12:11:04 -0400

    implementing save functions

--- src/main/java/com/djpompilio/textcrusaders/map.java
+++ src/main/java/com/djpompilio/textcrusaders/map.java
@@ -1,19 +1,31 @@
 package com.djpompilio.textcrusaders;
 
-public class map {
-    //worldmap 2d array 
-    void getMapData(){
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+// Map 2D array key: 1 = player   4 = test object
+
+
+public class map implements Serializable {
+    private int[][] worldMap = {{1, 0, 0, 0}, {0, 4, 0, 0}, {0, 4, 0, 0}, {0, 0, 0, 0}};
+
+    public int[][] getMapData(){
+        return worldMap;
+    }
+    void setPlayerLocation(int x, int y) {
 
     }
-    //playermap 2d array
-    void setPlayerLocation(){
+    void updateMap(int x, int y, int type) { //add locations to map etc.
 
     }
-    void getPlayerLocation(){
-
-    }
-
-    static void saveMap(){
+    public void saveMap() throws IOException, ClassNotFoundException {
+    FileOutputStream fileOutputStream = new FileOutputStream("map.bin");
+    ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+    objectOutputStream.writeObject(worldMap);
+    objectOutputStream.flush();
+    objectOutputStream.close();
 
     }
     

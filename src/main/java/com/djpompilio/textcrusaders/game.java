47415b9090899147c3dc21f5d411a7a172edc65d
djpompilio <djpompilio@gmail.com>
2025-05-31 12:10:46 -0400

    added

--- src/main/java/com/djpompilio/textcrusaders/game.java
+++ src/main/java/com/djpompilio/textcrusaders/game.java
@@ -1,7 +1,27 @@
 package com.djpompilio.textcrusaders;
 
 public class game {
-    static void saveGame(){
+    static void saveGame() {
+        
+
+    }
+
+
+    static void runGame(int[][] map, player p) {
+        textCrusaders.writeStoryLog("t");
+
+    }
+        //movement
+    void movePlayerNorth(int units){
+        //map.setPlayerLocation(units,0);
+    }
+    static void movePlayerSouth(int units){
+
+    }
+    static void movePlayerEast(int units){
+
+    }
+    static void movePlayerWest(int units){
 
     }
 }
